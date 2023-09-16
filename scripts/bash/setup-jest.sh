0244346c3c48f28b0a0edd66930b13b20ea6b828
max5961 <mmorrissey5961@gmail.com>
2023-09-16 01:22:21 -0400

    Modify script

--- scripts/bash/setup-jest.sh
+++ scripts/bash/setup-jest.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 mkdir src/__tests__
-npm install --save-dev jest babel-jest @babel/core @babel/preset-env @babel/preset-typescript @types/jest
+npm install --save-dev jest babel-jest @babel/core @babel/preset-env 
 
-echo ""
-echo "In order to run Jest with TypeScript:"
-echo "      add "@babel/preset-typescript" to "presets" in .babelrc"
-echo "      Example:"
-echo "      {
-            "presets": [
-                "@babel/preset-env",
-                "@babel/preset-typescript"
-            ]
-        }
-"
+echo "Use with TypeScript? [y/n]"
+read answer
+if [ "$answer" = "" ] || [ "$answer" = 'y' ] || [ "$answer" = 'Y' ]; then
+    npm install --save-dev @babel/preset-typescript @types/jest
+    echo ""
+    echo "In order to run Jest with TypeScript:"
+    echo "      add "@babel/preset-typescript" to "presets" in .babelrc"
+    echo "      Example:"
+    echo "      {
+                "presets": [
+                    "@babel/preset-env",
+                    "@babel/preset-typescript"
+                ]
+            }
+    "
+fi
