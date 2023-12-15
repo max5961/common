08a4c2ba8dd5df90728a775a2e404cfed63599f5
max5961 <mmorrissey5961@gmail.com>
2023-12-15 13:15:28 -0500

    Update/add stevearc aerial plugin

--- dotfiles/nvim/lua/src/plugins/telescope.lua
+++ dotfiles/nvim/lua/src/plugins/telescope.lua
@@ -1,10 +1,11 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
--- or                              , branch = '0.1.x',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.4',
+    -- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    
+
     -- set remaps
     config = function()
         local builtin = require('telescope.builtin')
@@ -12,5 +13,10 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "TelescopeResults",
+            command = "setlocal nofoldenable"
+        })
     end
 }
