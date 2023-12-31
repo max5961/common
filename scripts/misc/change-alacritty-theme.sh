e679eb647b9dce80c9420d52f9e818ecebb57bef
max5961 <mmorrissey5961@gmail.com>
2023-12-30 22:25:15 -0500

    Modify script

--- scripts/misc/change-alacritty-theme.sh
+++ scripts/misc/change-alacritty-theme.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-# provide at least one argument for grep to use to search for matching files
-if [ -z "${1}" ]; then
-    echo "Expected argument"
-    exit 1
-fi
-
-# to avoid remembering exact file names, multiple arguments may be used
-# grep pipes results of each argument into the next argument
 alacrittyConfigDir="$HOME/common/dotfiles/alacritty"
 themesDir="${alacrittyConfigDir}/dist/themes"
-results=$(ls "${themesDir}" | grep "${1}")
+
+# provide at least one argument
+# otherwise list available themes
+if [ -z "${1}" ] || [ "${1}" == "--themes" ]; then
+    ls "${themesDir}"
+    exit 0
+fi
+
+# search for themes with -g
+if [ "${1}" == "-g" ]; then
+    if [ -z "${2}" ]; then
+        echo "Expected search term"
+        exit 1
+    else
+        echo $(ls "${themesDir}" | grep "${2}")
+        exit 0
+    fi
+fi
+
+### main
+# to avoid remembering exact file names, multiple arguments may be used
+# grep pipes results of each argument into the next argument
+results=$(ls "${themesDir}" | grep -i "${1}")
 for arg in "${@}"; do
-    results=$(echo "${results}" | grep "${arg}")
+    results=$(echo "${results}" | grep -i "${arg}")
 done
 
 # if there are still multiple results, get just the first
