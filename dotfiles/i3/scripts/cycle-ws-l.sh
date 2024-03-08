9794223ab69d03d8eb3c0552a6fedb2c05bfb368
max5961 <mmorrissey5961@gmail.com>
2024-03-08 00:53:16 -0500

    Update

--- dotfiles/i3/scripts/cycle-ws-l.sh
+++ dotfiles/i3/scripts/cycle-ws-l.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# curr=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true) | .name')
-#
-# # current workspace is already at start
-# if ((curr == 1 || curr == 11)); then
-#     exit 0
-# fi
-#
-# n_top=
-# n_bottom=
-# if ((curr > 11)); then
-#     n_top=$((curr - 1))
-#     n_bottom=$((n_top - 10))
-# else
-#     n_bottom=$((curr - 1))
-#     n_top=$((n_bottom + 10))
-# fi
-#
-# i3-msg workspace number "$n_bottom"
-# i3-msg workspace number "$n_top"
-
 curr=$(cat /tmp/current-workspace-number)
 if ((curr == 1)); then
     exit 0
@@ -32,4 +12,7 @@ m2=$((curr + 9))
 echo "$m2: $m1"
 
 i3-msg workspace number "$m1"
-i3-msg workspace "$m2: $m1"
+if ! xrandr | grep 'HDMI-1-0 disconnected'; then
+    echo "cycle-ws-l: external connected"
+    i3-msg workspace "$m2: $m1"
+fi
--- dotfiles/i3/scripts/cycle-ws-r.sh
+++ dotfiles/i3/scripts/cycle-ws-r.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# curr=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true) | .name')
-
-# current workspace is already at end
-# if ((curr == 9 || curr == 19)); then
-#     exit 0
-# fi
-#
-# n_top=
-# n_bottom=
-# if ((curr > 10)); then
-#     n_top=$((curr + 1))
-#     n_bottom=$((n_top - 10))
-# else
-#     n_bottom=$((curr + 1))
-#     n_top=$((n_bottom + 10))
-# fi
-#
-# i3-msg workspace number "$n_bottom"
-# i3-msg workspace number "$n_top"
-
 curr=$(cat /tmp/current-workspace-number)
 if ((curr == 9)); then
     exit 0
@@ -31,4 +11,7 @@ m1=$((curr + 1))
 m2=$((curr + 11))
 
 i3-msg workspace number "$m1"
-i3-msg workspace "$m2: $m1"
+if ! xrandr | grep 'HDMI-1-0 disconnected'; then
+    echo "cycle-ws-r: external connected"
+    i3-msg workspace "$m2: $m1"
+fi
