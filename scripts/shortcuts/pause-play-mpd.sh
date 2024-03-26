7f8a96cec24069d9fc23582b32568b5083e995af
max5961 <mmorrissey5961@gmail.com>
2024-03-26 13:50:32 -0400

    Update

--- scripts/shortcuts/pause-play-mpd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if mpc | grep -q playing; then
-    mpc pause
-else
-    mpc play
-fi
--- templates/layouts/modal-sidebar/src/index.ts
+++
@@ -1,4 +0,0 @@
-import './style/index.scss';
-import { Layout } from './controller';
-
-(document.querySelector('button.show-sidebar') as HTMLButtonElement).onclick = Layout.showSidebar;
