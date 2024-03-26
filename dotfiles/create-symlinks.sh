627d5600e8e8f138d757b0b88f953bf350238ffd
max5961 <mmorrissey5961@gmail.com>
2024-03-26 13:41:03 -0400

    Modify format / add to create-symlinks script

--- dotfiles/create-symlinks.sh
+++ dotfiles/create-symlinks.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-# This script will not overwrite pre existing symlinks or normal files/dirs
-# Manually remove the config directories/files you want to symlink to
+# This script will not overwrite pre-existing symlinks or normal files/dirs.
+# Manually move the config directories or files to a folder in the $dotfiles_dir
+# and this script will symlink them to either $HOME/.config or $HOME
 
 dotfiles_dir="$HOME/common/dotfiles"
 
 function print_success_message() {
-    if [ "${1}" -eq 0 ]; then
-        echo "Successfully created symlink for ${2}"
+    if [ "$1" -eq 0 ]; then
+        echo "Successfully created symlink for $2"
     fi
 }
 
-# takes 1 argument: name of directory
+# accepts 1 argument: name of directory
 function link_dir_to_XDG_CONFIG_HOME() {
     path="$HOME/.config"
-    if [ -d "${path}/${1}" ] || [ -L "${path}/${1}" ]; then
+    if [ -d "$path/$1" ] || [ -L "$path/$1" ]; then
         echo "symlink not created - directory already exists in ${path}/${1}"
     else
-        ln -s "${dotfiles_dir}/${1}" ~/.config/${1}
-        print_success_message "${?}" "${1}"
+        ln -s "$dotfiles_dir/$1" "$HOME/.config/$1"
+        print_success_message "$?" "$1"
     fi
 
 }
 
-# takes 2 arguments: name of directory, name of file
+# accepts 2 arguments: name of directory, name of file
 function link_file_to_HOME() {
-    if [ -f "$HOME/${2}" ] || [ -L "$HOME/${2}" ]; then
-        echo "symlink not created - file already exists in $HOME/${2}"
+    if [ -f "$HOME/$2" ] || [ -L "$HOME/$2" ]; then
+        echo "symlink not created - file already exists in $HOME/$2"
     else
-        ln -s ${dotfiles_dir}/${1}/${2} ~/${2}
-        print_success_message "${?}" "${2}"
+        ln -s "$dotfiles_dir/$1/$2 $HOME/$2"
+        print_success_message "$?" "$2"
     fi
 }
 
@@ -43,6 +44,7 @@ link_dir_to_XDG_CONFIG_HOME "tmux"
 link_dir_to_XDG_CONFIG_HOME "i3"
 link_dir_to_XDG_CONFIG_HOME "lf"
 link_dir_to_XDG_CONFIG_HOME "rofi"
+link_dir_to_XDG_CONFIG_HOME "picom"
 
 # files in ~/
 link_file_to_HOME "bash" ".bashrc"
