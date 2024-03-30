02f107ce0ef9b07494cbb56cf4857843c7cb7507
max5961 <mmorrissey5961@gmail.com>
2024-03-30 02:19:34 -0400

    Fix color picker not loading commands

--- dotfiles/nvim/lua/plugins/color-picker.lua
+++ dotfiles/nvim/lua/plugins/color-picker.lua
@@ -2,9 +2,21 @@
 -- https://github.com/uga-rosa/ccc.nvim/blob/main/doc/ccc.txt
 
 return {
-    "uga-rosa/ccc.nvim",
-    config = function()
-        vim.api.nvim_set_keymap("n", "<leader>cp", "<cmd>CccPick<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("n", "<leader>cc", "<cmd>CccConvert<CR>", { noremap = true, silent = true })
-    end
+    {
+        "uga-rosa/ccc.nvim",
+        config = function()
+            require("ccc").setup {}
+            vim.api.nvim_set_keymap("n", "<leader>cp", "<cmd>CccPick<CR>", { noremap = true, silent = true })
+            vim.api.nvim_set_keymap("n", "<leader>cc", "<cmd>CccConvert<CR>", { noremap = true, silent = true })
+        end
+    },
+    -- {
+    --     "ziontee113/color-picker.nvim",
+    --     config = function()
+    --         vim.api.nvim_set_keymap("n", "<leader>cp", "<cmd>PickColor<CR>", { noremap = true, silent = true })
+    --     end
+    -- },
+    -- {
+    --     "max397574/colortils.nvim",
+    -- }
 }
