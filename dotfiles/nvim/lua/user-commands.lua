ac510dab623e2dac8620104accc14b912bcc6712
max5961 <mmorrissey5961@gmail.com>
2024-06-18 17:48:12 -0400

    Create open notes popup

--- dotfiles/nvim/lua/user-commands.lua
+++ dotfiles/nvim/lua/user-commands.lua
@@ -83,36 +83,68 @@ vim.api.nvim_create_user_command(
     {}
 )
 
--- opens a split window of notes as a way to quick reference
-local function openNotes()
-    local categories = { "bash", "cpp", "css", "git", "javascript", "linux-ref", "nodejs", "react", "scss",
-        "typescript", "vim" }
-    for i, v in ipairs(categories) do
-        print(i .. ": " .. v)
-    end
-    print(" ")
-
-    local category = vim.fn.input("Enter one of the above: ")
-
-    if "number" == type(tonumber(category)) then
-        if categories[tonumber(category)] then
-            openSplitWindow("~/notes/" .. categories[tonumber(category)] .. "/notes.txt")
-        else
-            print(" Invalid entry")
-        end
-    else
-        local isValid = false
-        for i, v in ipairs(categories) do
-            if category == v then
-                isValid = true
+-- opens ~/notes directory and then opens a split window with selection
+local function getNotesDirs()
+    -- get dirs from ~/notes
+    local dirs = {};
+    -- local fd = vim.loop.fs_scandir(vim.loop.os_homedir() .. "/notes");
+    local fd = vim.loop.fs_scandir(".")
+    if fd then
+        while true do
+            local name, typ = vim.loop.fs_scandir_next(fd)
+            if typ == "directory" then
+                table.insert(dirs, name);
+            end
+            if name == nil then
+                break
             end
         end
-        if isValid then
-            openSplitWindow("~/notes/" .. category .. "/notes.txt");
-        else
-            print(" Invalid entry")
-        end
     end
+
+    -- print for dev
+    return dirs
 end
 
-vim.api.nvim_create_user_command("Notes", openNotes, {})
+getNotesDirs()
+
+-- make popup and populate
+local popup = require("plenary.popup")
+
+local Win_id
+
+local function ListNotesInPopUp(opts, cb)
+    -- if height exceeds 5 lines, height will grow to fit
+    local height = 5
+    local width = 30
+    local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+    Win_id = popup.create(opts, {
+        title = "Notes",
+        highlight = "NotesWindow",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = 30,
+        minheight = 5,
+        borderchars = borderchars,
+        callback = cb,
+    })
+
+    local bufnr = vim.api.nvim_win_get_buf(Win_id)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua CloseNotesPopUp()<CR>", { silent = false })
+end
+
+local function displayNotes()
+    local opts = getNotesDirs()
+
+    local function cb(_, sel)
+        openSplitWindow("~/notes/" .. sel .. "/notes.txt")
+    end
+
+    ListNotesInPopUp(opts, cb)
+end
+
+function CloseNotesPopUp()
+    vim.api.nvim_win_close(Win_id, true)
+end
+
+vim.api.nvim_create_user_command("Notes", displayNotes, {});
