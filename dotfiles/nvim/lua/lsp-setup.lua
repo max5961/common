8083b6cff636300eaf4abb95d97bc90509483ed4
Maxwell Morrissey <mmorrissey92@proton.me>
2025-06-14 01:02:15 -0400

    lsp: overwrite gd default to pick first

--- dotfiles/nvim/lua/lsp-setup.lua
+++ dotfiles/nvim/lua/lsp-setup.lua
@@ -6,13 +6,26 @@ vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
 vim.api.nvim_create_autocmd("LspAttach", {
 	desc = "LSP actions",
 	callback = function(event)
+		-- TypeScript LSP was giving two options for gd class defs (name and constructor)
+		-- and multiple options are also generally annoying. (leader gd as a fallback
+		-- to the normal gd function)
+		local function pick_first()
+			vim.lsp.buf.definition({
+				on_list = function(t)
+					local loc = t.items[1].user_data
+					vim.lsp.util.show_document(loc, "utf-8")
+				end,
+			})
+		end
+
 		local opts = { buffer = event.buf }
 
 		-- these will be buffer-local keybindings
 		-- because they only work if you have an active language server
 
 		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover({border = 'rounded'})<cr>", opts)
-		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "gd", pick_first, opts)
+		vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
 		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
 		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
 		-- vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
