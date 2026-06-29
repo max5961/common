7fe9266d6eb1c90aff922117a2ee284960a7e706
Maxwell Morrissey <mmorrissey92@proton.me>
2026-06-29 18:59:49 -0400

    automated dev update

--- scripts/rofi/dmenu/rofi-notes.sh
+++ scripts/rofi/dmenu/rofi-notes.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
 NOTES="$HOME/Documents/notes"
-CONTENTS=$(ls -A "$NOTES" | sort )
 
-CHOSEN=$(echo -en "$CONTENTS" | rofimonitor -dmenu -i --matching fuzzy -p "Notes:")
+exec_fzf() {
+    cd "$NOTES"
+    chosen=$(fzf --style=full --layout=reverse --walker=file,follow)
+    [[ ! -z "${chosen}" ]] && "${editor}" "${chosen}"
+}
 
-cd "$NOTES/$CHOSEN"
-kitty --execute lf "$NOTES/$CHOSEN" 2>&1
+main() {
+    # default to konsole
+    local term="${TERMINAL:-konsole}"
+    local editor="$2"
 
+    if [[ ! -z "$1" ]]; then
+        exec_fzf
+        exit "$?"
+    fi
 
+    # script is recursive, $0 is the script path itself
+    # $EDITOR was not being recognized with konsole, so force var through as an argument
+    "$term" -e "${0}" --run-exec-fzf "$EDITOR" && exit "$?"
+}
+
+main "${@}"
 
